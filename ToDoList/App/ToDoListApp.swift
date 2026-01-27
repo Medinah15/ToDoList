5d6ab80c8d3855fc9e18c26ba2d35bbb8fe26898
Medina <medinahuseynova@hotmail.com>
2026-01-27 07:37:36 +0400

    refactor: prepare Core Data model for TodoList app

--- ToDoList/App/ToDoListApp.swift
+++ ToDoList/App/ToDoListApp.swift
@@ -13,7 +13,7 @@ struct ToDoListApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TodoListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
